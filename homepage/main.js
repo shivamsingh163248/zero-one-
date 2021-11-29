2cff193216a770b1d8249f1b5b23b2a1aab0c296
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2021-11-30 00:52:09 +0530

    Submit

---
+++ homepage/main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var x = document.getElementById("nevigation");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+
