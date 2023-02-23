e260cad3c5c20a9fad0a04ece4470e8555a062ae
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-02-24 01:12:13 +0530

    update new fature

--- profilecard1.js
+++ profilecard1.js
@@ -1,26 +1,26 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 1;
+showSlides4(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides4(slideIndex += n);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentSlide(n) {
+  showSlides4(slideIndex = n);
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+function showSlides4(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides_teacher");
+  let dots = document.getElementsByClassName("dott1");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-red", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-red";
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
